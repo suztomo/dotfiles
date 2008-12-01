2bcc78c4a0dedd69c38ff68de5c7b452d2319010
suztomo <suztomo@gmail.com>
2008-12-01 20:45:09 +0900

    gitignore added

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 ln -s $HOME/dotfiles/.emacs.el $HOME/.emacs.el
 ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
 
