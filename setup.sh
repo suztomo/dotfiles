0f62da10d2869450631ab981411e69fd0ee2db3b
suztomo <suztomo@gmail.com>
2008-12-03 23:37:06 +0900

    gdbinit added

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ ln -s $HOME/dotfiles/.emacs.el $HOME/.emacs.el
 ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
+ln -s $HOME/dotfiles/.gdbinit $HOME/.gdbinit
 
