a9a5b845741ef2c4aa477d22486c180a2ff9e460
suztomo <suztomo@gmail.com>
2008-12-14 17:30:22 +0900

    setup.sh has git config

--- setup.sh
+++ setup.sh
@@ -3,4 +3,17 @@ ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
 ln -s $HOME/dotfiles/.gdbinit $HOME/.gdbinit
+IFS=:
+GIT=false
+for d in $PATH
+do test -x $d/git && GIT=true
+done
+if $GIT
+then
+  echo "git found!"
+  git config --global user.name "suztomo"
+  git config --global user.email "suztomo@gmail.com"
+  git config --global core.excludeesfile "$HOME/.gitignore"
+else echo "no git"
+fi
 
